111fd5a4fad38a5b89bb20d35ed4f7fd50a21d72
Cheshnark <csnark.dev@gmail.com>
2024-01-22 19:57:42 +0100

    Update Accordion to include Amplipod.

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -3,12 +3,11 @@ import './Accordion.css';
 import greenShot from '../../images/green-shot.PNG'; 
 import drinkCookShot from '../../images/drink-cook-shot.PNG';
 import rocasecaShot from '../../images/rocaseca-shot.PNG';
+import amplipodShot from '../../images/amplipod-shot.PNG';
 
 const Accordion = () => {
-
     const changeShow = (e:any) => {
         e.target.nextSibling.classList.toggle("show")
-        
     }
     
     return (
@@ -17,21 +16,22 @@ const Accordion = () => {
                 <h3 onClick={changeShow}>Green</h3>
                 <div className="expandable">
                     <div className="expandable-img">
-                        <a href="https://green-vert.vercel.app/" target="_blank"><img src={greenShot} alt="" /></a>
+                        <a href="https://green-vert.vercel.app/" target="_blank"><img src={greenShot} alt="Green site image" /></a>
                     </div>
                     <p>Green fue mi primer proyecto realizado en React y usa la librería de componentes Material UI. Es una 
                         app sencilla que cuenta con una página principal, portfolio, blog y un formulario de subscripción. 
                         Cuenta además de con un backend ligero hecho con Express. 
                         <br /><br />
                         La idea era crear una página web para una pequeña
-                        empresa o freelance que tuviese todo lo necesario para defender su presencia online.</p>
+                        empresa o freelance que tuviese todo lo necesario para defender su presencia online.
+                    </p>
                 </div>
             </div>
             <div className="drink-and-cook">
                 <h3 onClick={changeShow}>Drink & Cook</h3>
                 <div className="expandable">
                     <div className="expandable-img">
-                            <a href="https://drink-cook.vercel.app/" target="_blank"><img src={drinkCookShot} alt="" /></a>
+                            <a href="https://drink-cook.vercel.app/" target="_blank"><img src={drinkCookShot} alt="Drink & Cook site image" /></a>
                     </div>
                     <p>En este proyecto junte dos cosas que me gustan (la cerveza y cocinar) para practicar con el uso de APIs y
                          cómo trabajar con ellas desde el backend. Está hecho también en React y en esta ocasión usé vanilla css y
@@ -39,14 +39,15 @@ const Accordion = () => {
                          <br /><br />
                          La aplicación permite revisar el catálogo de la cervecera BrewDog y empareja cada cerveza con una receta
                          que le vaya bien y las instrucciones para prepararla. Las APIs que consume son la de la propia BrewDog y 
-                         la de Tasty para las recetas.</p>
+                         la de Tasty para las recetas.
+                    </p>
                 </div>
             </div>
             <div className="rocaseca">
                 <h3 onClick={changeShow}>Rocaseca</h3>
                 <div className="expandable">
                     <div className="expandable-img">
-                        <a href="https://rocaseca.vercel.app/" target="_blank"><img src={rocasecaShot} alt="" /></a>
+                        <a href="https://rocaseca.vercel.app/" target="_blank"><img src={rocasecaShot} alt="Rocaseca site image" /></a>
                     </div>
                     <p>Rocaseca es mi proyecto más ambicioso hasta la fecha. Otra de las cosas que me gusta mucho es la escalada y aquí
                          quería crear una solución centralizada para mirar el tiempo en las escuelas de escalada (por ahora cercanas a
@@ -58,6 +59,21 @@ const Accordion = () => {
                          <br /><br />
                          Para protejer los sectores de escalada, solo podrás buscarlos si ya los conoces. Si no estás familiarizado con este 
                          mundo prueba escribiendo "Manzanares el Real" o "Guadalix" en el buscador.
+                    </p>
+                </div>
+            </div>
+            <div className="amplipod">
+                <h3 onClick={changeShow}>Amplipod</h3>
+                <div className="expandable">
+                    <div className="expandable-img">
+                        <a href="https://app.amplipod.io" target="_blank"><img src={amplipodShot} alt="Amplipod site image" /></a>
+                    </div>
+                    <p>
+                        Amplipod es el proyecto en el que trabajé dentro de Zeonec. Amplipod es un servicio para podcast que mediante el uspo de IA generativa permite optimizar toda la parte de marketing de los episodios de un podcast.
+                         <br /><br />
+                         La app permite tanto subir un episodio desde nuestro equipo como encontrarlo mediante un buscador incorporado. Una vez hecho esto, obtendremos tanto la transcripción del episodio como resultados optimizados para marketing (Instagram, Linkedin, keywords, blog...). Además, permite la creación de imágenes relacionadas con el episodio y videos con subtítulos para la promoción del capítulo en redes. 
+                         <br /><br />
+                         A nivel técnico, algunas de las tecnologías usadas para construir el sitio son: Next.js 13, Express.js, Django, Tailwind o Supabase.
                     </p>
                 </div>
             </div>
